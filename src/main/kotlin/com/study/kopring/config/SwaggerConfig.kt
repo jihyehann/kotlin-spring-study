3722566da693af4e1fd100bd3d42bf5156de4cc8
Han Jihye <haneh0714@gmail.com>
2022-11-15 18:51:48 +0900

    DOCS : Swagger.md 수정 - 인증 헤더 관련 설정 설명 추가

--- src/main/kotlin/com/study/kopring/config/SwaggerConfig.kt
+++ src/main/kotlin/com/study/kopring/config/SwaggerConfig.kt
@@ -2,17 +2,15 @@ package com.study.kopring.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpHeaders.AUTHORIZATION
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
-import springfox.documentation.service.ApiKey
-import springfox.documentation.service.AuthorizationScope
-import springfox.documentation.service.SecurityReference
-import springfox.documentation.service.SecurityScheme
+import springfox.documentation.service.*
+import springfox.documentation.service.HttpAuthenticationScheme.JWT_BEARER_BUILDER
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spi.service.contexts.SecurityContext
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
-import java.util.*
 
 
 @Configuration
@@ -24,7 +22,7 @@ class SwaggerConfig {
         return Docket(DocumentationType.OAS_30)
             .useDefaultResponseMessages(false)
             .securityContexts(listOf(securityContext()))
-            .securitySchemes(listOf(apiKey()))
+            .securitySchemes(listOf(httpScheme()))
             .select()
             .apis(RequestHandlerSelectors.any())
             .paths(PathSelectors.any())
@@ -37,15 +35,24 @@ class SwaggerConfig {
             .build();
     }
 
-    private fun defaultAuth(): List<SecurityReference?> {
+    private fun defaultAuth(): List<SecurityReference> {
         val authorizationScope = AuthorizationScope("global", "accessEverything")
         val authorizationScopes: Array<AuthorizationScope?> = arrayOfNulls(1)
         authorizationScopes[0] = authorizationScope
-        return listOf(SecurityReference("Authorization", authorizationScopes))
+        return listOf(SecurityReference(AUTHORIZATION, authorizationScopes))
     }
 
-    private fun apiKey(): ApiKey {
-        return ApiKey("Authorization", "Authorization", "header");
-    }
+    // ApiKey를 사용하는 방법
+    private fun apiKey(): ApiKey = ApiKey("Authorization", "Authorization", "header")
+
+    // Bearer 방식을 사용하는 방법
+    private fun httpScheme(): HttpAuthenticationScheme = HttpAuthenticationScheme(
+        /* name = */ AUTHORIZATION,
+        /* description = */ "jwt 토큰",
+        /* type = */ "http",
+        /* scheme = */ JWT_BEARER_BUILDER.build().scheme,
+        /* bearerFormat = */ JWT_BEARER_BUILDER.build().bearerFormat,
+        /* extensions = */ ArrayList()
+    )
 
 }
